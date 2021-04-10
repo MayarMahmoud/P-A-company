6c9cf537b36faf786fe4318ffe953bd5d98f3a02
MayarMahmoud <Mayar.Khalifa@student.guc.edu.eg>
2021-04-10 18:16:59 +0200

    adj

--- p-a-website-v1/src/components/pages/HomePage/Data.js
+++ p-a-website-v1/src/components/pages/HomePage/Data.js
@@ -11,44 +11,44 @@ export const homeObjOne = {
     img: 'images/svg-1.svg',
     alt: 'Credit Card'
   };
-  export const homeObjTwo = {
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'Vision',
-    headline: '',
-    description:
-      'The vision of the company is not only become one of the most reliable suppliers and biggest firms in the region but also create a big family between us and the customers with maintaining an exquisite level of professionalism.',
-    buttonLabel: 'Learn More',
-    imgStart: '',
-    img: 'images/svg-5.svg',
-    alt: 'Vault'
-  };
+  // export const homeObjTwo = {
+  //   lightBg: false,
+  //   lightText: true,
+  //   lightTextDesc: true,
+  //   topLine: 'Vision',
+  //   headline: '',
+  //   description:
+  //     'The vision of the company is not only become one of the most reliable suppliers and biggest firms in the region but also create a big family between us and the customers with maintaining an exquisite level of professionalism.',
+  //   buttonLabel: 'Learn More',
+  //   imgStart: '',
+  //   img: 'images/svg-5.svg',
+  //   alt: 'Vault'
+  // };
   
   export const homeObjThree = {
     lightBg: true,
     lightText: false,
     lightTextDesc: false,
-    topLine: 'EASY SETUP',
-    headline: 'Super fast and simple onboarding process',
+    topLine: 'Vision',
+    headline: '',
     description:
-      "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+    'The vision of the company is not only become one of the most reliable suppliers and biggest firms in the region but also create a big family between us and the customers with maintaining an exquisite level of professionalism.',
     buttonLabel: 'Start Now',
     imgStart: 'start',
     img: 'images/svg-7.svg',
     alt: 'Vault'
   };
   
-  export const homeObjFour = {
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
-    topLine: 'DATA ANALYTICS',
-    headline: 'Every transaction is stored on our secure cloud database',
-    description:
-      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-    buttonLabel: 'Sign Up Now',
-    imgStart: 'start',
-    img: 'images/svg-8.svg',
-    alt: 'Vault'
-  };
+  // export const homeObjFour = {
+  //   lightBg: false,
+  //   lightText: true,
+  //   lightTextDesc: true,
+  //   topLine: 'DATA ANALYTICS',
+  //   headline: 'Every transaction is stored on our secure cloud database',
+  //   description:
+  //     'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+  //   buttonLabel: 'Sign Up Now',
+  //   imgStart: 'start',
+  //   img: 'images/svg-8.svg',
+  //   alt: 'Vault'
+  // };
